d2eb5db218bd21b70b8d7874a28cf8dd1c1f4e81
p <p@localhost.localdomain>
2021-06-05 15:37:37 +0200

    Improvements on test_package

--- test_package/example.cpp
+++ test_package/example.cpp
@@ -3,6 +3,8 @@
 #include <tins/network_interface.h>
 #include <tins/sniffer.h>
 #include <tins/pdu.h>
+#include <tins/rawpdu.h>
+#include <tins/utils.h>
 //
 int main() {
     //hello();
@@ -18,8 +20,23 @@ int main() {
         
         if(some_pdu)
         {
+            std::cout << "Sniffed a random packet: pdu_type: " <<  Tins::Utils::to_string(some_pdu->pdu_type()) << std::endl;
             
-            std::cout << "Sniffed a random packet" << std::endl;
+            const Tins::RawPDU *raw = some_pdu->find_pdu<Tins::RawPDU>();
+            std::cout <<"RawPDU header_size: " << raw->header_size() << std::endl;
+            std::cout <<"RawPDU payload_size: " << raw->payload_size() << std::endl;
+            std::cout <<"RawPDU payload().size(): " << raw->payload().size() << std::endl;
+            std::vector<uint8_t> buffer = raw->payload();
+            std::cout << std::endl;
+
+            for (int i =0; i < buffer.size(); i++ ){
+                std::cout << std::hex << static_cast<int>(buffer[i]);
+                std::cout << " ";
+            }
+            std::cout << std::endl;
+            
+
+
             delete some_pdu;        
             std::cout << "Success" << std::endl;
             return EXIT_SUCCESS;
